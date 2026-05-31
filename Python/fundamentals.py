047189d29bb9c5a226003877188d92f583bdb3c6
Sivakrishna <sivakrishna.g@muraai.com>
2026-05-31 14:14:33 +0530

    python fundamentals

---
+++ Python/fundamentals.py
@@ -0,0 +1,45 @@
+# Printing to console
+# print("Learning python fundamentals")
+# For Multi line comments, there is no /* ... */ like in java, we need to put # in front of each line
+
+# Variables in python
+name = "Gedela Sivakrishna"
+roll = 242
+age = 23
+gender = "Male"
+isWorking = True
+# print(name, roll, age, gender, isWorking)
+
+# Printing variable types
+# print(name, type(name))
+# print(age, type(age))
+# print(isWorking, type(isWorking))
+
+# Constants - there is no keyword to define constants, they can be changed.
+# General convention to use uppercase to define a constant.
+PI = 3.14
+
+# Datatypes
+# 1. int
+salary = 30000
+
+# 2. float
+cgpa = 8.7
+
+# 3. str
+college = "Centurion university of technology and management"
+
+# 4. bool
+isAdult = True
+isMarried = False
+
+# 5. None - represents this variable doesn't hold any value
+someVariable = None
+
+# 6. list
+skills = ["Java", "JavaScript", "HTML", "CSS", "React", "Spring"]
+# add another skill
+skills.append("Angular")
+# print(skills)
+
+# 7. tuple
