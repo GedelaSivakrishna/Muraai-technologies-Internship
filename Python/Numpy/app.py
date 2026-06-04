7f5c45ca3c9a825ff9101513dba0a6ac6eaf35c9
Sivakrishna <sivakrishna.g@muraai.com>
2026-06-04 13:20:42 +0530

    Numpy Day 3

--- Python/Numpy/app.py
+++ Python/Numpy/app.py
@@ -32,12 +32,12 @@ arrayFromList = np.array([list1, list2])
 # print(arrayFromList[1, 2]) # 1st row, 2nd element = 6
 
 # Array attributes
-# 1. ndim - this property returns whether the array is 1D, 2D, 3D .... ND
+# 1. data.ndim - this property returns whether the array is 1D, 2D, 3D .... ND
 # print(a)
 # print(a.ndim)
 # print(arrayFromList.ndim)
 
-# 2. shape - represents how many elements in each layer
+# 2. data.shape - represents how many elements in each layer
 # [
 #     [
 #         [1, 2, 3]
@@ -53,36 +53,36 @@ arrayFromList = np.array([list1, list2])
 # print(len(arrayFromList.shape)) # 3 - this returns what dimension this array is 1D, 2D, ....
 # print(a.shape) # 1 * 5
 
-# 3. size - returns the count of total elements in the array
+# 3. data.size - returns the count of total elements in the array
 # print(a.size) # 5
 # print(arrayFromList.size) # 6
 # print(math.prod(a.shape)) # 1 * 5 = 5
 # print(math.prod(arrayFromList.shape)) # 2 * 1 * 3 = 6
 
-# 4. dtype - returns the datatype of array
+# 4. data.dtype - returns the datatype of array
 # print(a.dtype) # int
 # print(arrayFromList.dtype) # int
 
 # Creating Basic Array
-# 1. Creates array filled with zeros
+# 1. np.zeros() - Creates array filled with zeros
 zerosArray = np.zeros(10)
 # print(zerosArray)
 
-# 2. Creates array filled with ones
+# 2. np.ones() - Creates array filled with ones
 onesArray = np.ones(10)
 # print(onesArray)
 
-# 3. Creates empty array with random values - this operation is faster than zeros
+# 3. np.empty() - Creates empty array with random values - this operation is faster than zeros
 emptyArray = np.empty(10)
 # print(emptyArray)
 
-# 4. Creates array within a range of values
+# 4. np.arange() - Creates array within a range of values
 rangeArray = np.arange(10)
 # print(rangeArray)
 rangeArrayWithStepSize = np.arange(1, 10, 2)
 # print(rangeArrayWithStepSize)
 
-# 5. Creates an array with specified values with equal gap b/w the start & stop
+# 5. np.linspace() - Creates an array with specified values with equal gap b/w the start & stop
 equalSpacedArray = np.linspace(1, 10, 5)
 # print(equalSpacedArray)
 
@@ -93,17 +93,17 @@ intEmptyArray = np.empty(10, dtype = np.int64) # fills random values
 
 # Adding, removing and sorting elements
 unsortedArray = np.array([5, 9, 1, -2, 4, 10])
-# 1. sorts the array
+# 1. np.sort() - sorts the array
 sortedArray = np.sort(unsortedArray)
 # print(sortedArray)
 
-# 2. concatenates the array
+# 2. np.concatenate() - concatenates the array
 arr1 = np.array([20, 10])
 arr2 = np.array([30, 50])
 mergedArray = np.concatenate((arr1, arr2))
 # print(mergedArray)
 
-# Reshape an array
+# data.reshape() - Reshapes an array
 reshapedArray = a.reshape(5, 1)
 # print(a)
 # print(reshapedArray)
@@ -123,7 +123,7 @@ a3 = a[:, np.newaxis] # adds one dimension at second position and makes the arra
 # 3. Column vector - array in the shape (n * 1) is said to be column vector. The array should
 #    have only 1 column to qualify as column vector.
 
-# np.expand_dims: Expand the dimension of the array
+# np.expand_dims() - Expand the dimension of the array
 b1 = np.expand_dims(a, axis=0)
 # print(b1)
 b2 = np.expand_dims(a, axis=1)
@@ -160,17 +160,17 @@ slicedArray = arr[1:5] # creates an array from original with slice
 slicedArray[0] = 200 #  changes are reflected in original array
 # print(arr)
 # print(slicedArray) 
-# vstack - vertically stacks the arrays
+# np.vstack() - vertically stacks the arrays
 a1 = np.array([[1,1],
                [2,2]])
 a2 = np.array([[3,3],
                [4,4]])
 verticallyStackedArray = np.vstack((a1, a2))
 # print(verticallyStackedArray)
-# hstack - horizantally stacks the arrays
+# np.hstack() - horizantally stacks the arrays
 horizantallyStackedArray = np.hstack((a1, a2))
 # print(horizantallyStackedArray)
-# hsplit - splits the array horizantally
+# np.hsplit() - splits the array horizantally
 x = np.arange(1, 25).reshape(2, 12)
 # print(x)
 # print(np.hsplit(x, 3))
@@ -224,3 +224,89 @@ b = np.array([[0.45053314, 0.17296777, 0.34376245, 0.5510652],
 # print(b.sum()) # by default operates on all axis
 # print(b.sum(axis=0)) # we can specify along which axis we want the operation to be performed
 
+# Creating Matrices
+matrix = np.array([[11, 2],
+                   [13, 4],
+                   [1, 16]])
+# print(matrix)
+# accessing matrix elements
+# print(matrix[0, 0])
+# slicing matrix
+# print(matrix[1:])
+# print(matrix[0:2, 0])
+# By default, aggregate functions operate in all axis and dimensions
+# print(matrix.min())
+# print(matrix.max())
+# print(matrix.sum())
+# We can specify the range in which we want the aggregate functions to be applied
+# print(matrix.max(axis=0))
+# print(matrix.max(axis=1))
+# Mathmatical operations on matrix
+onesArray = np.ones(dtype=int, shape=(3, 2))
+# print(onesArray)
+# print(matrix + onesArray)
+# print(matrix - onesArray)
+# print(matrix * onesArray)
+# print(matrix / onesArray)
+# print(np.ones(shape=(4, 5, 2), dtype=int))
+# print(np.random.random((3, 3)))
+
+# Generating Random Numbers
+# print(np.random.randint(1, 100, 5)) # Generate random numbers b/w low(inclusive) & high(exclusive)
+# # third parameter is the size
+# print(np.random.randint(1, 10, size=(3, 3))) # Generate multidimensional random array
+
+# Get unique items and count
+duplicateValuesArray = np.array([1, 1, 2, 3, 2, 5, 4, -1, 3, 5, -2])
+# print(np.unique(duplicateValuesArray))
+# return the index of unique elements in the array
+# print(np.unique(duplicateValuesArray, return_index=True))
+# return_counts - returns the occurance of elements in the array 
+unique_values, occurance_counts = np.unique(duplicateValuesArray, return_counts=True) 
+# print(unique_values)
+# print(occurance_counts)
+duplicate_2D_array = np.array([[1, 2, 3, 4, 1],
+                               [5, 6, 7, 8, 5],
+                               [9, 10, 11, 12, 9],
+                               [1, 2, 3, 4, 1]])
+# print(np.unique(duplicate_2D_array)) # 2D array is flattened & sorted in ascending order
+# flattening means converting a multidimensional array to 1 dimensional array
+# flattens the 2D array
+# print(duplicate_2D_array.flatten())
+# unique also works with axis in multidimensional
+unique_rows = np.unique(duplicate_2D_array, axis=0)
+# print(unique_rows)
+unique_cols = np.unique(duplicate_2D_array, axis=1)
+# print(unique_cols)
+
+# Transposing and reshaping a matrix
+# data.reshape() - changes the dimensions of the matrix
+reshaped_2D_array = duplicate_2D_array.reshape(5, 4)
+# print(reshaped_2D_array)
+# data.transpose() / data.T - returns the transpose of the matrix
+# print(duplicate_2D_array.transpose())
+# print(duplicate_2D_array.T)
+
+# Reverse an array
+# np.flip() - reverses the array along all axis by default
+# print(np.flip(a))
+# print(np.flip(duplicate_2D_array))
+# reversing around axis=0
+# print(np.flip(duplicate_2D_array, axis=0))
+# reversing around axis=1
+# print(np.flip(duplicate_2D_array, axis=1))
+# reversing only one row
+# duplicate_2D_array[2] = np.flip(duplicate_2D_array[2])
+# print(duplicate_2D_array)
+# reverse only one column
+duplicate_2D_array[:, 0] = np.flip(duplicate_2D_array[:, 0])
+# print(duplicate_2D_array)
+
+# Reshapping and flattening multidimensional arrays
+# data.flatten() & data.ravel() - helps in flattening the multidimensional array
+# data.flatten() does deep copy while data.ravel() doesn't so changes reflect in original array
+arr = np.array([[1,2,3,4],
+                [5,6,7,8],
+                [9,10,11,12]])
+# print(arr.flatten())
+# print(arr.ravel())
